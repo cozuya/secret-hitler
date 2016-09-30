657e5867987610dab917f7da6a6303d9c5f8cc25
Chris Ozols <chris.v.ozols@gmail.com>
2016-09-30 17:17:56 -0500

    more startgame

--- routes/socket/game/start-game.js
+++ routes/socket/game/start-game.js
@@ -2,28 +2,25 @@ const {sendInProgressGameUpdate} = require('../util.js'),
 	_ = require('lodash');
 
 module.exports = game => {
-	game.gameState.isStarted = true;
-	game.private.seatedPlayers = game.seatedPlayers = _.shuffle(game.seatedPlayers);
-	game.general.status = 'Dealing roles..';
-	sendInProgressGameUpdate(game);
-
-	const roles = _.range(0, 3).map((el) => {
+	let roles = _.range(0, 3).map(el => {
 		return {
 			roleName: 'liberal',
 			icon: el,
 			team: 'liberal'
 		};
 	}).concat([{
-			roleName: 'fascist',
-			icon: 0,
-			team: 'fascist'
-		},
+		roleName: 'fascist',
+		icon: 0,
+		team: 'fascist'
+	},
 		{
 			roleName: 'hitler',
 			icon: 0,
 			team: 'fascist'
 		}]
 	);
+
+	game.general.type = 0; // different fascist tracks
 
 	if (game.seatedPlayers.length > 5) {
 		roles = roles.concat([{
@@ -38,7 +35,8 @@ module.exports = game => {
 			roleName: 'fascist',
 			icon: 1,
 			team: 'fascist'
-		}]);		
+		}]);
+		game.general.type = 1;
 	}
 
 	if (game.seatedPlayers.length > 7) {
@@ -54,7 +52,8 @@ module.exports = game => {
 			roleName: 'fascist',
 			icon: 2,
 			team: 'fascist'
-		}]);		
+		}]);
+		game.general.type = 2;
 	}
 
 	if (game.seatedPlayers.length > 9) {
@@ -64,4 +63,23 @@ module.exports = game => {
 			team: 'liberal'
 		}]);
 	}
+
+	game.gameState.isStarted = true;
+	game.private.seatedPlayers = game.seatedPlayers = _.shuffle(game.seatedPlayers);
+	game.general.status = 'Dealing roles..';
+	game.private.seatedPlayers.forEach(player => {
+		const index = Math.floor(Math.random() * roles.length);
+
+		player.role = roles[index];
+		roles.splice(index, 1);
+		player.playersState.forEach(play => {
+			play.cardStatus = {
+				cardDisplayed: true,
+				isFlipped: false,
+				cardFront: 'secretrole',
+				cardBack: ''
+			};
+		});
+	});
+	sendInProgressGameUpdate(game);
 };
