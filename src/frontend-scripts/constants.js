73731f5a0dd5a26e50fcb01e369ba71b23c463c2
Chris Ozols <chris.v.ozols@gmail.com>
2017-08-03 21:16:42 -0500

    finished grow refactoring dealy feature

--- src/frontend-scripts/constants.js
+++ src/frontend-scripts/constants.js
@@ -4,7 +4,7 @@ const MODERATORS = module.exports.MODERATORS = ['sethe', 'maki2', 'Crazyuncle', 
 
 const ADMINS = module.exports.ADMINS = ['coz', 'Stine'];
 
-const CONTRIBUTORS = module.exports.CONTRIBUTORS = ['jbasrai', 'sethe', 'veggiemanz'];
+const CONTRIBUTORS = module.exports.CONTRIBUTORS = ['jbasrai', 'sethe', 'veggiemanz', 'DFinn'];
 
 // const NATEKILLER = 'Banana';
 
