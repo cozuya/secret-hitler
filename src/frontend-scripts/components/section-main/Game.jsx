b138867adf870a9b82b5d06abf322461855c62c4
Chris Ozols <chris.v.ozols@gmail.com>
2017-11-04 20:16:40 -0500

    (#303)

--- src/frontend-scripts/components/section-main/Game.jsx
+++ src/frontend-scripts/components/section-main/Game.jsx
@@ -77,6 +77,7 @@ export default class Game extends React.Component {
 
 					if (
 						userInfo.userName &&
+						!userInfo.gameSettings.disableConfetti &&
 						gameInfo &&
 						gameInfo.publicPlayersState &&
 						gameInfo.publicPlayersState.find(player => player.userName === userInfo.userName) &&
