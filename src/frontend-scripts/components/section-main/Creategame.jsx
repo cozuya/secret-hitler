15d32b09184dda12ab5bc87a11d2475d97256420
andy013 <andy01389@blueyonder.co.uk>
2017-11-15 21:28:49 +0000

    Fix for create game slider on touch devices

--- src/frontend-scripts/components/section-main/Creategame.jsx
+++ src/frontend-scripts/components/section-main/Creategame.jsx
@@ -952,11 +952,17 @@ export default class Creategame extends React.Component {
 
 	render() {
 		const sliderCheckboxClick = index => {
-			if (!this.state.sliderValues.includes(index + 5)) {
+				const newSliderValues = this.state.checkedSliderValues.map((el, i) => (i === index ? !el : el));
 				this.setState({
-					checkedSliderValues: this.state.checkedSliderValues.map((el, i) => (i === index ? !el : el))
+					checkedSliderValues: newSliderValues
 				});
-			}
+				const includedPlayerCounts = newSliderValues.map((el, i) => el ? i + 5 : null).filter(el => el !== null);
+				const min = Math.min(...includedPlayerCounts);
+				const max = Math.max(...includedPlayerCounts);
+
+				this.setState({
+					sliderValues: [min, max]
+				});
 		};
 
 		return (
@@ -1009,14 +1015,13 @@ export default class Creategame extends React.Component {
 					<div className="row slider">
 						<div className="eight wide column centered slider">
 							<h4 className="ui header">Number of players</h4>
-							<Range onChange={this.sliderChange} min={5} max={10} defaultValue={[5, 10]} marks={{ 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10' }} />
+							<Range onChange={this.sliderChange} min={5} max={10} defaultValue={[5, 10]} value={this.state.sliderValues} marks={{ 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10' }} />
 							<div className="checkbox-container">
 								{new Array(6).fill(true).map((el, index) => (
 									<label key={index}>
 										<input
 											type="checkbox"
 											checked={this.state.checkedSliderValues[index]}
-											disabled={index + 5 <= this.state.sliderValues[0] || index + 5 >= this.state.sliderValues[1]}
 											onChange={() => {
 												sliderCheckboxClick(index);
 											}}
