15190ce63520dc8d6ae30773848a52c111b2caac
LordVader <lordvadermm9988@gmail.com>
2018-03-10 11:03:32 -0600

    Hot Module Replacement Ideas?

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,8 +1,27 @@
 const path = require('path');
 const Reload = require('webpack-livereload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: './src/frontend-scripts/game-app.js',
+	entry: {
+		'./src/frontend-scripts/game-app.js',
+		app: './src/index.js'
+	},
+    	devtool: 'inline-source-map',
+    	devServer: {
+      		contentBase: './dist',
+     		hot: true
+    	},
+    	plugins: [
+      		new CleanWebpackPlugin(['dist']),
+      		new HtmlWebpackPlugin({
+        		title: 'Hot Module Replacement'
+      		}),
+     		new webpack.NamedModulesPlugin(),
+     		new webpack.HotModuleReplacementPlugin()
+    	],
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, '../public/scripts')
