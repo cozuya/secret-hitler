51fa8b51647e1afcdf573683a527748e95bb4312
LordVader <LordVader@secret-hitler.org>
2018-03-25 23:00:33 -0500

    Update constants.js

--- src/frontend-scripts/constants.js
+++ src/frontend-scripts/constants.js
@@ -20,6 +20,7 @@ const EDITORS = (module.exports.EDITORS = ['Max', 'cbell', 'Faaiz1999', 'Invidia
 const ADMINS = (module.exports.ADMINS = ['coz', 'Stine']);
 const CONTRIBUTORS = (module.exports.CONTRIBUTORS = [
 	'Auengun',
+	'OuchYouHitMe',
 	'Skyrra',
 	'jbasrai',
 	'sethe',
