4717f1c4e5eb2f79c408f8a07e0a1c68710899cc
Hexicube <marc@firstlinetech.co.uk>
2018-04-26 20:23:33 +0100

    Fix for the "rescind and retry" remake bug

--- routes/socket/user-events.js
+++ routes/socket/user-events.js
@@ -1070,6 +1070,7 @@ module.exports.handleUpdatedRemakeGame = (passport, game, data) => {
 
 		newGame.chats = [];
 		newGame.general.isRemade = false;
+		newGame.general.isRemaking = false;
 		newGame.general.uid = `${game.general.uid}Remake`;
 		newGame.general.electionCount = 0;
 		newGame.timeCreated = new Date().getTime();
