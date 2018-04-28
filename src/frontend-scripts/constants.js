a98ce95dc1e318774c5805482b07a8798486560e
NthTensor <nth.tensor@gmail.com>
2018-04-28 12:46:06 -0500

    Fixed inexperienced players getting color

--- src/frontend-scripts/constants.js
+++ src/frontend-scripts/constants.js
@@ -69,22 +69,26 @@ module.exports.PLAYERCOLORS = (user, isSeasonal, defaultClass) => {
 		const w = isSeasonal ? user[`winsSeason${CURRENTSEASONNUMBER}`] : user.wins;
 		const l = isSeasonal ? user[`lossesSeason${CURRENTSEASONNUMBER}`] : user.losses;
 
-		return cn(defaultClass, {
-			experienced1: w + l > 49,
-			experienced2: w + l > 99,
-			experienced3: w + l > 199,
-			experienced4: w + l > 299,
-			experienced5: w + l > 499,
-			onfire1: w / (w + l) > 0.52,
-			onfire2: w / (w + l) > 0.54,
-			onfire3: w / (w + l) > 0.56,
-			onfire4: w / (w + l) > 0.58,
-			onfire5: w / (w + l) > 0.6,
-			onfire6: w / (w + l) > 0.62,
-			onfire7: w / (w + l) > 0.64,
-			onfire8: w / (w + l) > 0.66,
-			onfire9: w / (w + l) > 0.68,
-			onfire10: w / (w + l) > 0.7
-		});
+		if (w + l >= 50) {
+			return cn(defaultClass, {
+				experienced1: w + l > 49,
+				experienced2: w + l > 99,
+				experienced3: w + l > 199,
+				experienced4: w + l > 299,
+				experienced5: w + l > 499,
+				onfire1: w / (w + l) > 0.52,
+				onfire2: w / (w + l) > 0.54,
+				onfire3: w / (w + l) > 0.56,
+				onfire4: w / (w + l) > 0.58,
+				onfire5: w / (w + l) > 0.6,
+				onfire6: w / (w + l) > 0.62,
+				onfire7: w / (w + l) > 0.64,
+				onfire8: w / (w + l) > 0.66,
+				onfire9: w / (w + l) > 0.68,
+				onfire10: w / (w + l) > 0.7
+			});
+		} else {
+			return defaultClass;
+		}
 	}
 };
