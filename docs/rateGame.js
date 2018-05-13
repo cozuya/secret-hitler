0cc68b42a2839b9e6c514f3e288f8d3316d67667
Chris Ozols <chris.v.ozols@gmail.com>
2018-05-12 20:13:42 -0500

    cleaned up dev dependencies, some work on rategame but doesnt work because I'm dumb

--- docs/rateGame.js
+++ docs/rateGame.js
@@ -1,17 +1,60 @@
 const Game = require('../models/game'); // temp
 const Account = require('../models/account'); // temp
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost:15726/secret-hitler-app`);
 
 // const rateGame = () => {
-Game.findOne({ uid: 'MelodicAfraidSwallow' }).then(g => {
+Game.findOne({ uid: 'MelodicAfraidSwallow' }).then(game => {
+	console.log('found a game');
 	const liberalsWon = game.winningTeam === 'liberal';
 
+	// async function getAverageElos(accounts) {
+	// 	const accountElos = await Account.find({ username: { $in: accounts } }).then(acc => {
+	// 		const elos = acc.map(a => a.eloOverall || 1600);
+	// 		console.log(elos, 'elos');
+	// 		const avg = elos.reduce((prev, curr) => prev + curr, 0) / elos.length;
+	// 		console.log(avg, 'avg');
+	// 		return avg;
+	// 	});
+
+	// 	console.log(accountElos, 'acelos');
+
+	// 	return accountElos;
+	// }
+
 	async function getAverageElos(accounts) {
-		await Account.find({ username: { $in: accounts } }).then(acc => {
-			console.log(acc, 'acc');
-		});
+		// const accelo = await Account.find({ username: { $in: accounts } }).then(acc => {
+		// 	const elos = acc.map(a => a.eloOverall || 1600);
+		// 	console.log(elos, 'elos');
+		// 	const avg = elos.reduce((prev, curr) => prev + curr, 0) / elos.length;
+		// 	console.log(avg, 'avg');
+		// 	return avg;
+		// });
+
+		// console.log(accelo, 'accelo');
+
+		// return Promise.resolve(accelo);
+
+		return Account.find({ username: { $in: accounts } });
+
+		// console.log(accountElos, 'acelos');
+
+		// return accountElos;
 	}
 
-	const liberalsAverageElo = getAverageElos(liberalsWon ? game.winningPlayers : game.losingPlayers);
+	const liberalsAverageElo = getAverageElos(
+		liberalsWon ? game.winningPlayers.map(player => player.userName) : game.losingPlayers.map(player => player.userName)
+	).then(acc => {
+		const elos = acc.map(a => a.eloOverall || 1600);
+		console.log(elos, 'elos');
+		const avg = elos.reduce((prev, curr) => prev + curr, 0) / elos.length;
+		console.log(avg, 'avg');
+		return avg;
+	});
+
 	console.log(liberalsAverageElo, 'libelo');
+	console.log(liberalsAverageElo / 2, 'div');
 });
 // }
