e6472f52e9f7dca7b283a9d41bed4a11da896a62
Chris Ozols <chris.v.ozols@gmail.com>
2018-05-13 14:57:56 -0500

    rategame formula mostly in though seeing some weird discrepencies re: seasonal

--- docs/rateGame.js
+++ docs/rateGame.js
@@ -1,6 +1,7 @@
 const Game = require('../models/game'); // temp
 const Account = require('../models/account'); // temp
 const mongoose = require('mongoose');
+const { CURRENTSEASONNUMBER } = require('../src/frontend-scripts/constants');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://localhost:15726/secret-hitler-app`);
@@ -16,57 +17,62 @@ const libWinAdjust = {
 
 let gameCount = 0;
 
-Game.findOne({})
-	.limit(50000)
+Game.findOne({}, { chats: 0 })
+	// .skip(170000)
+	// .limit(2)
 	.cursor()
 	.eachAsync(game => {
 		const winningPlayerNames = game.winningPlayers.map(player => player.userName);
 		const losingPlayerNames = game.losingPlayers.map(player => player.userName);
 
-		Account.find({ username: { $in: winningPlayerNames.concat(losingPlayerNames) } }, { eloOverall: 1, eloSeason: 1, username: 1 }).then(accounts => {
-			const liberalsWon = game.winningTeam === 'liberal';
+		Account.find({ username: { $in: winningPlayerNames.concat(losingPlayerNames) } }, { eloOverall: 1, eloSeason: 1, username: 1 })
+			.then(accounts => {
+				let averageRatingWinners =
+					winningPlayerNames.reduce(
+						(prev, curr) =>
+							accounts.find(account => account.username === curr) ? accounts.find(account => account.username === curr).eloOverall + prev : 1600,
+						0
+					) / winningPlayerNames.length;
+				let averageRatingLosers =
+					losingPlayerNames.reduce(
+						(prev, curr) =>
+							accounts.find(account => account.username === curr) ? accounts.find(account => account.username === curr).eloOverall + prev : 1600,
+						0
+					) / losingPlayerNames.length;
 
-			let averageRatingWinners =
-				winningPlayerNames.reduce((prev, curr) => (accounts.find(account => account.username === curr).eloOverall || 1600) + prev, 0) /
-				winningPlayerNames.length;
-			let averageRatingLosers =
-				losingPlayerNames.reduce((prev, curr) => (accounts.find(account => account.username === curr).eloOverall || 1600) + prev, 0) / losingPlayerNames.length;
+				if (game.winningTeam === 'liberal') {
+					averageRatingWinners += libWinAdjust[game.playerCount];
+				} else {
+					averageRatingLosers += libWinAdjust[game.playerCount];
+				}
 
-			if (liberalsWon) {
-				averageRatingWinners += libWinAdjust[game.playerCount];
-			} else {
-				averageRatingLosers += libWinAdjust[game.playerCount];
-			}
+				// double p = 1.0 / (1.0 + Math.pow(10.0, (avgRatingWinners - avgRatingLosers) / 400.0));
 
-			// double p = 1.0 / (1.0 + Math.pow(10.0, (avgRatingWinners - avgRatingLosers) / 400.0));
+				const k = 64;
+				const p = 1 / (1 + Math.pow(10, (averageRatingWinners - averageRatingLosers) / 400));
 
-			const k = 64;
-			const p = 1 / (1 + 10 ** ((averageRatingWinners - averageRatingLosers) / 400));
-			const winningPlayerAdjustment = k * p / winningPlayerNames.length;
-			const losingPlayerAdjustment = -k * p / losingPlayerNames.length;
+				const winningPlayerAdjustment = k * p / winningPlayerNames.length;
+				const losingPlayerAdjustment = -k * p / losingPlayerNames.length;
 
-			// if (Number.isInteger(gameCount / 50)) {
-			// 	console.log(winningPlayerAdjustment, 'win');
-			// 	console.log(losingPlayerAdjustment, 'lose');
-			// }
+				accounts.forEach(account => {
+					account.eloOverall = winningPlayerNames.includes(account.username)
+						? (account.eloOverall || 1600) + winningPlayerAdjustment
+						: (account.eloOverall || 1600) + losingPlayerAdjustment;
 
-			accounts.forEach(account => {
-				account.eloOverall = winningPlayerNames.includes(account.username)
-					? (account.eloOverall || 1600) + winningPlayerAdjustment
-					: (account.eloOverall || 1600) + losingPlayerAdjustment;
+					if (game.season === CURRENTSEASONNUMBER) {
+						account.eloSeason = winningPlayerNames.includes(account.username)
+							? (account.eloSeason || 1600) + winningPlayerAdjustment
+							: (account.eloSeason || 1600) + losingPlayerAdjustment;
+					}
 
-				account.save();
+					account.save();
+				});
+			})
+			.catch(err => {
+				console.log(err, 'caught err');
 			});
-
-			// 		double k = 64;
-
-			// // Update player ratings
-			// updateRatings(game.getWinningPlayers(), k * p / game.getWinningPlayers().size());
-			// updateRatings(game.getLosingPlayers(), -k * p / game.getLosingPlayers().size());
-		});
 		gameCount++;
 		if (Number.isInteger(gameCount / 100)) {
 			console.log('processed game ' + gameCount);
 		}
 	});
-// }
---
+++ scripts/assignBaseElo.js
@@ -0,0 +1,21 @@
+const Account = require('../models/account'); // temp
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost:15726/secret-hitler-app`);
+
+let count = 0;
+
+Account.findOne({})
+	.cursor()
+	.eachAsync(account => {
+		account.eloSeason = account.eloOverall = 1600;
+		account.save();
+		count++;
+		if (Number.isInteger(count / 100)) {
+			console.log('processed account ' + count);
+		}
+	})
+	.catch(err => {
+		console.log(err, 'caught err');
+	});
