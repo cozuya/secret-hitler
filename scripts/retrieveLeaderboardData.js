c71386b8222e7ceb694ea8d396c9327a1a2a75d4
Chris Ozols <chris.v.ozols@gmail.com>
2018-06-09 15:21:23 -0500

    leaderboard component implemented

--- scripts/retrieveLeaderboardData.js
+++ scripts/retrieveLeaderboardData.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Account = require('../models/account');
-// const moment = require('moment');
-// const _ = require('lodash');
 const fs = require('fs');
 const data = {
 	seasonalLeaderboard: [],
@@ -11,23 +9,31 @@ const data = {
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://localhost:15726/secret-hitler-app`);
 
-Account.find({})
+Account.find({ 'games.1': { $exists: true } })
 	.cursor()
 	.eachAsync(account => {
-		if (account.previousDayElo) {
-			data.dailyLeaderboard.push({
+		if (account.account.previousDayElo > 1620) {
+			data.seasonalLeaderboard.push({
 				userName: account.username,
-				dailyEloDifference: account.eloSeason - account.previousDayElo
+				elo: account.eloSeason
 			});
 		}
 		account.previousDayElo = account.eloSeason;
+		account.save();
 	})
 	.then(() => {
 		Account.find({ lastCompletedGame: { $gte: new Date(Date.now() - 86400000) } })
 			.cursor()
-			.eachAsync(account => {})
+			.eachAsync(account => {
+				data.dailyLeaderboard.push({
+					userName: account.username,
+					dailyEloDifference: account.eloSeason - account.previousDayElo
+				});
+			})
 			.then(() => {
-				fs.writeFile('/var/www/secret-hitler/data/leaderboardData.json', JSON.stringify(data), () => {
+				data.dailyLeaderboard = data.dailyLeaderboard.sort((a, b) => b.dailyEloDifference - a.dailyEloDifference).slice(0, 20);
+				data.seasonalLeaderboard = data.seasonalLeaderboard.sort((a, b) => b.elo - a.elo).slice(0, 20);
+				fs.writeFile('/var/www/secret-hitler/public/leaderboardData.json', JSON.stringify(data), () => {
 					mongoose.connection.close();
 				});
 			});
--- src/frontend-scripts/components/section-main/Leaderboards.jsx
+++ src/frontend-scripts/components/section-main/Leaderboards.jsx
@@ -12,16 +12,56 @@ class Leaderboard extends React.Component {
 	}
 
 	componentDidMount() {
+		const self = this;
+
 		$.ajax({
-			url: 'data/leaderboardData.json',
+			// url: 'leaderboardData.json',
+			url: '../temp.json',
 			success(data) {
-				console.log(data, 'd');
+				self.setState({
+					seasonalLeaderboard: data.seasonalLeaderboard,
+					dailyLeaderboard: data.dailyLeaderboard
+				});
 			}
 		});
 	}
 
 	render() {
-		return <section className="changelog">hi</section>;
+		return (
+			<section className="leaderboards">
+				<a href="#/">
+					<i className="remove icon" style={{ marginTop: '30px' }} />
+				</a>
+				<div className="ui grid">
+					<div className="eight wide column">
+						<h2 className="ui header">Seasonal leaders</h2>
+						<ul>
+							{this.state.seasonalLeaderboard.map(user => (
+								<li key={user.userName}>
+									<p>
+										<a href={`#/profile/${user.userName}`}>{user.userName}</a>
+									</p>
+									<p>{user.elo.toFixed(0)}</p>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className="eight wide column">
+						<h2 className="ui header">Daily leaders</h2>
+						<ul>
+							{this.state.dailyLeaderboard.map(user => (
+								<li key={user.userName}>
+									<p>
+										<a href={`#/profile/${user.userName}`}>{user.userName}</a>
+									</p>
+									<p>+{user.dailyEloDifference.toFixed(0)}</p>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</section>
+		);
 	}
 }
 
