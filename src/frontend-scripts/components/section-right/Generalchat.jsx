f4776a574f59aeb776a3ff63c44adfcee2345463
LordVader <lordvadermm9988@gmail.com>
2018-09-29 15:22:33 -0500

    removed discord integration in general chat
    since widget bot shutdown permanently

--- src/frontend-scripts/components/section-right/Generalchat.jsx
+++ src/frontend-scripts/components/section-right/Generalchat.jsx
@@ -217,7 +217,7 @@ export default class Generalchat extends React.Component {
 		const { userInfo } = this.props;
 		const discordIconClick = () => {
 			this.setState({
-				discordEnabled: !this.state.discordEnabled
+				discordEnabled: this.state.discordEnabled
 			});
 		};
 
@@ -231,21 +231,12 @@ export default class Generalchat extends React.Component {
 							className={this.state.lock ? 'large lock icon' : 'large unlock alternate icon'}
 							onClick={this.handleChatLockClick}
 						/>
-						{userInfo &&
-							userInfo.userName && (
-								<img
-									title="Click to show our discord general chat instead of the site's general chat"
-									className={this.state.discordEnabled ? 'active discord-icon' : 'discord-icon'}
-									src="/images/discord-icon.png"
-									onClick={discordIconClick}
-								/>
-							)}
 					</div>
 				</section>
 				<section className="segment chats">
 					{!this.state.discordEnabled && this.renderSticky()}
 					{this.state.discordEnabled ? (
-						<embed height="100%" width="100%" src="https://widgetbot.io/embed/323243744914571264/323243744914571264/0003/" />
+						<embed height="100%" width="100%" src="https://discord.gg/secrethitlerio" />
 					) : (
 						<Scrollbars ref={c => (this.scrollbar = c)} onScroll={this.handleChatScrolled}>
 							<div className="ui list genchat-container">{this.renderChats()}</div>
