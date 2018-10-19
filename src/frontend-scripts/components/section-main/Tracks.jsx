e6274f9aaed50433a0dfa64ba183a45501bd0eaf
Chris Ozols <chris.v.ozols@gmail.com>
2018-10-18 20:21:31 -0500

    minor fixes

--- src/frontend-scripts/components/section-main/Tracks.jsx
+++ src/frontend-scripts/components/section-main/Tracks.jsx
@@ -110,15 +110,11 @@ class Tracks extends React.Component {
 		let eloMinimum;
 		let eloMinimumTooltip;
 		let customgameactive;
-		const customgameactiveTooltip = "Custom Game";
-
+		const customgameactiveTooltip = 'Custom Game';
 
 		if (gameInfo.customGameSettings && gameInfo.customGameSettings.enabled) {
-			customgameactive = (
-				<i className="setting icon" />
-			);
-		}
-		else {
+			customgameactive = <i className="setting icon" />;
+		} else {
 			game.rebalance6p && game.rebalance7p && game.rebalance9p
 				? ((rebalance69p = <div> R679 </div>), (rebalance69pTooltip = 'Rebalanced 6, 7, & 9 player games'))
 				: game.rebalance6p && game.rebalance7p
@@ -202,11 +198,12 @@ class Tracks extends React.Component {
 
 		return (
 			<div className="options-icons-container">
-				{gameInfo.customGameSettings && gameInfo.customGameSettings.enabled &&
-					<span className="customgame">
-						<Popup inverted trigger={customgameactive} content={customgameactiveTooltip} />
-					</span>
-				}
+				{gameInfo.customGameSettings &&
+					gameInfo.customGameSettings.enabled && (
+						<span className="customgame">
+							<Popup inverted trigger={customgameactive} content={customgameactiveTooltip} />
+						</span>
+					)}
 				{rebalance69p && (
 					<span className="rebalanced">
 						<Popup inverted trigger={rebalance69p} content={rebalance69pTooltip} />
@@ -312,12 +309,12 @@ class Tracks extends React.Component {
 				}, this.state.remakeStatus ? 10000 : 2000);
 			}
 		};
-		
+
 		const renderFasTrack = () => {
 			if (gameInfo.customGameSettings && gameInfo.customGameSettings.enabled) {
 				const offX = -8;
 				const offY = -8;
-				
+
 				let powers = [];
 				let numFas = 0;
 				let hzStart = 3;
@@ -332,7 +329,7 @@ class Tracks extends React.Component {
 						if (p == 'deckpeek') return 'Peek';
 						if (p == 'election') return 'Elect';
 						if (p == 'bullet') return 'Gun';
-		
+
 						console.log(`Unknown power: ${p}`);
 						return null;
 					});
@@ -340,65 +337,179 @@ class Tracks extends React.Component {
 					hzStart = gameInfo.customGameSettings.hitlerZone;
 					vzPoint = gameInfo.customGameSettings.vetoZone;
 					hitKnowsFas = gameInfo.customGameSettings.hitKnowsFas;
-				}
-				else {
+				} else {
 					// Should only happen before a game starts, but as a precaution typical settings are used.
 					if (gameInfo.general.playerCount < 7) {
 						powers = ['None', 'None', 'Peek', 'Gun', 'Gun'];
 						numFas = 1;
 						hitKnowsFas = true;
-					}
-					else if (gameInfo.general.playerCount < 9) {
+					} else if (gameInfo.general.playerCount < 9) {
 						powers = ['None', 'Inv', 'Elect', 'Gun', 'Gun'];
 						numFas = 2;
-					}
-					else {
+					} else {
 						powers = ['Inv', 'Inv', 'Elect', 'Gun', 'Gun'];
 						numFas = 3;
 					}
 				}
-				
+
 				const getHZ = pos => {
 					if (pos < hzStart) return 'Off';
 					if (pos > hzStart) return 'On';
 					return 'Start';
 				};
-				
+
 				return (
-					<div className='track bottom-track-back custom-fastrack-base'>
-						<span style={{width:'92px', height:'120px', left:`${offX+137}px`, top:`${offY+58}px`, position:'absolute', 'background-image':`url(../images/customtracks/fasTrackHZ${getHZ(1)}.png)`}} />
-						<span style={{width:'92px', height:'120px', left:`${offX+229}px`, top:`${offY+58}px`, position:'absolute', 'background-image':`url(../images/customtracks/fasTrackHZ${getHZ(2)}.png)`}} />
-						<span style={{width:'92px', height:'120px', left:`${offX+321}px`, top:`${offY+58}px`, position:'absolute', 'background-image':`url(../images/customtracks/fasTrackHZ${getHZ(3)}.png)`}} />
-						<span style={{width:'92px', height:'120px', left:`${offX+413}px`, top:`${offY+58}px`, position:'absolute', 'background-image':`url(../images/customtracks/fasTrackHZ${getHZ(4)}.png)`}} />
-						<span style={{width:'92px', height:'120px', left:`${offX+505}px`, top:`${offY+58}px`, position:'absolute', 'background-image':`url(../images/customtracks/fasTrackHZ${getHZ(5)}.png)`}} />
-						
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+ 58}px`,top:`${offY+58}px`,'background-image':`url(../images/customtracks/fasPower${powers[0]}${hzStart<=0?'Light':''}.png)`}}>
-							{vzPoint == 1 && <span className={'custom-fastrack-powerslot ' + ((hzStart<=0)?'custom-fastrack-vetozone-light':'custom-fastrack-vetozone')} />}
+					<div className="track bottom-track-back custom-fastrack-base">
+						<span
+							style={{
+								width: '92px',
+								height: '120px',
+								left: `${offX + 137}px`,
+								top: `${offY + 58}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrackHZ${getHZ(1)}.png)`
+							}}
+						/>
+						<span
+							style={{
+								width: '92px',
+								height: '120px',
+								left: `${offX + 229}px`,
+								top: `${offY + 58}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrackHZ${getHZ(2)}.png)`
+							}}
+						/>
+						<span
+							style={{
+								width: '92px',
+								height: '120px',
+								left: `${offX + 321}px`,
+								top: `${offY + 58}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrackHZ${getHZ(3)}.png)`
+							}}
+						/>
+						<span
+							style={{
+								width: '92px',
+								height: '120px',
+								left: `${offX + 413}px`,
+								top: `${offY + 58}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrackHZ${getHZ(4)}.png)`
+							}}
+						/>
+						<span
+							style={{
+								width: '92px',
+								height: '120px',
+								left: `${offX + 505}px`,
+								top: `${offY + 58}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrackHZ${getHZ(5)}.png)`
+							}}
+						/>
+
+						<span
+							className="custom-fastrack-powerslot"
+							style={{
+								left: `${offX + 58}px`,
+								top: `${offY + 58}px`,
+								backgroundImage: `url(../images/customtracks/fasPower${powers[0]}${hzStart <= 0 ? 'Light' : ''}.png)`
+							}}
+						>
+							{vzPoint == 1 && (
+								<span className={'custom-fastrack-powerslot ' + (hzStart <= 0 ? 'custom-fastrack-vetozone-light' : 'custom-fastrack-vetozone')} />
+							)}
 						</span>
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+150}px`,top:`${offY+58}px`,'background-image':`url(../images/customtracks/fasPower${powers[1]}${hzStart<=1?'Light':''}.png)`}}>
-							{vzPoint == 2 && <span className={'custom-fastrack-powerslot ' + ((hzStart<=1)?'custom-fastrack-vetozone-light':'custom-fastrack-vetozone')} />}
+						<span
+							className="custom-fastrack-powerslot"
+							style={{
+								left: `${offX + 150}px`,
+								top: `${offY + 58}px`,
+								backgroundImage: `url(../images/customtracks/fasPower${powers[1]}${hzStart <= 1 ? 'Light' : ''}.png)`
+							}}
+						>
+							{vzPoint == 2 && (
+								<span className={'custom-fastrack-powerslot ' + (hzStart <= 1 ? 'custom-fastrack-vetozone-light' : 'custom-fastrack-vetozone')} />
+							)}
 						</span>
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+242}px`,top:`${offY+58}px`,'background-image':`url(../images/customtracks/fasPower${powers[2]}${hzStart<=2?'Light':''}.png)`}}>
-							{vzPoint == 3 && <span className={'custom-fastrack-powerslot ' + ((hzStart<=2)?'custom-fastrack-vetozone-light':'custom-fastrack-vetozone')} />}
+						<span
+							className="custom-fastrack-powerslot"
+							style={{
+								left: `${offX + 242}px`,
+								top: `${offY + 58}px`,
+								backgroundImage: `url(../images/customtracks/fasPower${powers[2]}${hzStart <= 2 ? 'Light' : ''}.png)`
+							}}
+						>
+							{vzPoint == 3 && (
+								<span className={'custom-fastrack-powerslot ' + (hzStart <= 2 ? 'custom-fastrack-vetozone-light' : 'custom-fastrack-vetozone')} />
+							)}
 						</span>
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+334}px`,top:`${offY+58}px`,'background-image':`url(../images/customtracks/fasPower${powers[3]}${hzStart<=3?'Light':''}.png)`}}>
-							{vzPoint == 4 && <span className={'custom-fastrack-powerslot ' + ((hzStart<=3)?'custom-fastrack-vetozone-light':'custom-fastrack-vetozone')} />}
+						<span
+							className="custom-fastrack-powerslot"
+							style={{
+								left: `${offX + 334}px`,
+								top: `${offY + 58}px`,
+								backgroundImage: `url(../images/customtracks/fasPower${powers[3]}${hzStart <= 3 ? 'Light' : ''}.png)`
+							}}
+						>
+							{vzPoint == 4 && (
+								<span className={'custom-fastrack-powerslot ' + (hzStart <= 3 ? 'custom-fastrack-vetozone-light' : 'custom-fastrack-vetozone')} />
+							)}
 						</span>
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+426}px`,top:`${offY+58}px`,'background-image':`url(../images/customtracks/fasPower${powers[4]}${hzStart<=4?'Light':''}.png)`}}>
-							{vzPoint == 5 && <span className={'custom-fastrack-powerslot ' + ((hzStart<=4)?'custom-fastrack-vetozone-light':'custom-fastrack-vetozone')} />}
+						<span
+							className="custom-fastrack-powerslot"
+							style={{
+								left: `${offX + 426}px`,
+								top: `${offY + 58}px`,
+								backgroundImage: `url(../images/customtracks/fasPower${powers[4]}${hzStart <= 4 ? 'Light' : ''}.png)`
+							}}
+						>
+							{vzPoint == 5 && (
+								<span className={'custom-fastrack-powerslot ' + (hzStart <= 4 ? 'custom-fastrack-vetozone-light' : 'custom-fastrack-vetozone')} />
+							)}
 						</span>
-						<span className='custom-fastrack-powerslot' style={{left:`${offX+518}px`,top:`${offY+58}px`,'background-image':'url(../images/customtracks/fasPowerEndGame.png)'}} />
-						
-						<span style={{width:'268px', height:'15px', left:`${offX+336}px`, top:`${offY+60}px`, position:'absolute', 'background-image':'url(../images/customtracks/fasTrackHZText.png)'}}></span>
-						
-						<span style={{width:'228px', height:'11px', left:`${offX+220}px`, top:`${offY+186}px`, position:'absolute',
-														   'background-image':`url(../images/customtracks/fasTrack${numFas}fas.png)`}} />
-						<span style={{width:'228px', height:'11px', left:`${offX+220}px`, top:`${offY+196}px`, position:'absolute',
-														   'background-image':`url(../images/customtracks/fasTrack${(numFas>1)?'Multi':'Single'}${hitKnowsFas?'Known':'Unknown'}.png)`}} />
+						<span
+							className="custom-fastrack-powerslot"
+							style={{ left: `${offX + 518}px`, top: `${offY + 58}px`, backgroundImage: 'url(../images/customtracks/fasPowerEndGame.png)' }}
+						/>
+
+						<span
+							style={{
+								width: '268px',
+								height: '15px',
+								left: `${offX + 336}px`,
+								top: `${offY + 60}px`,
+								position: 'absolute',
+								backgroundImage: 'url(../images/customtracks/fasTrackHZText.png)'
+							}}
+						/>
+
+						<span
+							style={{
+								width: '228px',
+								height: '11px',
+								left: `${offX + 220}px`,
+								top: `${offY + 186}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrack${numFas}fas.png)`
+							}}
+						/>
+						<span
+							style={{
+								width: '228px',
+								height: '11px',
+								left: `${offX + 220}px`,
+								top: `${offY + 196}px`,
+								position: 'absolute',
+								backgroundImage: `url(../images/customtracks/fasTrack${numFas > 1 ? 'Multi' : 'Single'}${hitKnowsFas ? 'Known' : 'Unknown'}.png)`
+							}}
+						/>
 					</div>
 				);
-			}
-			else {
+			} else {
 				return (
 					<div
 						className={(() => {
