72a51973877948195001b406d0eb5e083f1a9938
Chris Ozols <chris.v.ozols@gmail.com>
2018-11-26 18:30:21 -0600

    lint

--- routes/socket/ip-obfuscator-v6.js
+++ routes/socket/ip-obfuscator-v6.js
@@ -6,22 +6,21 @@ for (let a = 0; a < 16 * 16 * 16 * 16; a++) avail[a] = convertToHex(a);
 
 // Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 const shuffleArray = array => {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = array[i];
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
-    	}
-}
+	}
+};
 shuffleArray(avail);
 
 for (let i = 0; i < avail.length; i += 2) {
 	const val1 = avail[i];
-	const val2 = avail[i+1];
+	const val2 = avail[i + 1];
 	map[val1] = val2;
 	map[val2] = val1;
 }
-
 
 const obfBlock = number => {
 	if (map[number] === undefined) throw new Error(`Invalid IP: ${number}`);
@@ -31,7 +30,8 @@ module.exports.obfBlock = obfBlock; // For testing purposes, should not be used 
 
 module.exports.obfIP = ip => {
 	const data = ip.split(':');
-	return data.slice(0, 8)
+	return data
+		.slice(0, 8)
 		.map(obfBlock)
-		.join(":");
+		.join(':');
 };
