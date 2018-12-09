120cf4cb11ba66783b8d59f84d2dfdf676b06570
Chris Ozols <chris.v.ozols@gmail.com>
2018-12-09 17:26:07 -0600

    helper script

---
+++ scripts/assignLocalToAllAccountsPriorToOauth.js
@@ -0,0 +1,25 @@
+const Account = require('../models/account'); // temp
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost:27017/secret-hitler-app`);
+
+let count = 0;
+
+Account.findOne({})
+	.cursor()
+	.eachAsync(account => {
+		account.isLocal = true;
+		account.save();
+		count++;
+		if (Number.isInteger(count / 1000)) {
+			console.log('processed account ' + count);
+		}
+	})
+	.then(() => {
+		console.log('done');
+		monogoose.connection.close();
+	})
+	.catch(err => {
+		console.log(err, 'caught err');
+	});
