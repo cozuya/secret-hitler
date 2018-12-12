918a65093e097e4463eda2391183ed1e625ce447
Spyro <44094305+Spyro9955@users.noreply.github.com>
2018-12-12 12:39:37 -0600

    fix 7

--- src/frontend-scripts/components/section-main/DisplayLobbies.jsx
+++ src/frontend-scripts/components/section-main/DisplayLobbies.jsx
@@ -411,8 +411,7 @@ const DisplayLobbies = props => {
 						<div className="gamename-column">
 							{renderFlag()}
 							{game.name}
-							{userInfo.isTrialMod) && <span style={{ color: 'lightblue' }}>{` Created by: ${game.gameCreatorName}`}</span>}
-							{userInfo.staffRole && <span style={{ color: 'lightblue' }}>{` Created by: ${game.gameCreatorName}`}</span>}
+							{(userInfo.isTrialMod || userInfo.staffRole) && <span style={{ color: 'lightblue' }}>{` Created by: ${game.gameCreatorName}`}</span>}
 						</div>
 						<div className="options-column experienced">{optionIcons()}</div>
 					</div>
