751831040b7c0fe5a8fb249f267f9ea6fac030ce
Chris Ozols <chris.v.ozols@gmail.com>
2019-01-23 16:44:01 -0600

    disabled istyping

--- src/frontend-scripts/components/section-main/Gamechat.jsx
+++ src/frontend-scripts/components/section-main/Gamechat.jsx
@@ -167,19 +167,19 @@ class Gamechat extends React.Component {
 				});
 			}
 		}
-		const now = new Date().getTime();
+		// const now = new Date().getTime();
 
-		if (userInfo.isSeated && (now - isTyping[userInfo.userName] > 1000 || !isTyping[userInfo.userName])) {
-			updateTyping({
-				...isTyping,
-				[userInfo.userName]: now
-			});
-			socket.emit('updateTyping', {
-				userName: userInfo.userName,
-				lastTypingTime: now,
-				uid: gameInfo.general.uid
-			});
-		}
+		// if (userInfo.isSeated && (now - isTyping[userInfo.userName] > 1000 || !isTyping[userInfo.userName])) {
+		// 	updateTyping({
+		// 		...isTyping,
+		// 		[userInfo.userName]: now
+		// 	});
+		// 	socket.emit('updateTyping', {
+		// 		userName: userInfo.userName,
+		// 		lastTypingTime: now,
+		// 		uid: gameInfo.general.uid
+		// 	});
+		// }
 	};
 
 	chatDisabled = () => {
