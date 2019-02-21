89a05172a411a9585ef855b84fbdbc0a9ac7c3b2
RPYoshi <RPYoshi@gmail.com>
2019-02-21 03:23:22 -0500

    Quick fix to make chat transtion better

--- src/frontend-scripts/chatanimation.js
+++ src/frontend-scripts/chatanimation.js
@@ -10,13 +10,19 @@ export default () => {
 	$('body').on('click', '#chatsidebar', function(event) {
 		event.preventDefault();
 
+	if (window.location.href.indexOf('#/table/') == -1)
+	{
 		$('.section-right').transition({
 			animation: 'slide left',
 			displayType: 'flex'
 		});
+	}
+	else
+	{
 		$('.chat-container').transition({
 			animation: 'slide left',
 			displayType: 'flex'
 		});
+	}
 	});
 };
