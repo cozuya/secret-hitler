98c2a7e8e2dc99ad2e9a4a6c53c19dff4b1ba7d8
Vignesh Joglekar <rexvigasaurus@gmail.com>
2019-02-24 16:01:32 -0600

    Fixes ELO slider new player 2000 ELO bug (#1249)

--- src/frontend-scripts/components/section-main/Creategame.jsx
+++ src/frontend-scripts/components/section-main/Creategame.jsx
@@ -573,8 +573,8 @@ export default class Creategame extends React.Component {
 		let player = null;
 		if (userList.list) player = userList.list.find(p => p.userName === userInfo.userName);
 		const isSeason = (userInfo.gameSettings && !userInfo.gameSettings.disableSeasonal) || false;
-		const playerElo = (player && Math.min(2000, player.eloSeason)) || 1600;
-		const playerEloNonseason = (player && Math.min(2000, player.eloOverall)) || 1600;
+		const playerElo = (player && player.eloSeason && Math.min(2000, player.eloSeason)) || 1600;
+		const playerEloNonseason = (player && player.eloOverall && Math.min(2000, player.eloOverall)) || 1600;
 		const max = Math.max(playerElo, playerEloNonseason);
 		const marks = Object.keys(origMarks)
 			.filter(k => origMarks[k] <= max)
