6389c2e36ed56bb9f3d87596257168d328985991
Vignesh Joglekar <rexvigasaurus@gmail.com>
2019-03-25 21:18:22 -0500

    Gives Contributor AEM their contributor color if they disable staff color

--- src/frontend-scripts/constants.js
+++ src/frontend-scripts/constants.js
@@ -53,7 +53,7 @@ export const PLAYERCOLORS = (user, isSeasonal, defaultClass, eloDisabled) => {
 		});
 	} else if (
 		user.isContributor &&
-		(!(user.staffRole && user.staffRole.length && user.staffRole !== 'trialmod' && user.staffRole !== 'altmod') || !user.staffDisableStaffColor)
+		(!(user.staffRole && user.staffRole.length && user.staffRole !== 'trialmod' && user.staffRole !== 'altmod') || user.staffDisableStaffColor)
 	) {
 		return cn(defaultClass, 'contributor');
 	} else {
