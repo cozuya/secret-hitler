7a15150878659e21de450e6a37e8427345c15b1a
Vignesh Joglekar <rexvigasaurus@gmail.com>
2019-03-26 23:35:12 -0500

    Update Changelog.jsx

--- src/frontend-scripts/components/section-main/Changelog.jsx
+++ src/frontend-scripts/components/section-main/Changelog.jsx
@@ -11,9 +11,8 @@ class Changelog extends React.Component {
 					<h2>Changelog</h2>
 				</div>
 				<div className="ui header">
-					<p>Version 1.3.1 released 4-1-2019</p>
+					<p>Version 1.4.0 released 4-1-2019</p>
 				</div>
-				<h3>Season 6 begins!</h3>
 				<h4>Many Moderation Improvements -Vigasaurus</h4>
 				<h4>Silent Game Features now work as intended -Vigasaurus</h4>
 				<h4>Returning feature: Grey and Private-Only players are alerted when they try to join games that they can't -samf</h4>
