eea4140d5a8666a7e699f711f0b601e85020702c
Vignesh Joglekar <rexvigasaurus@gmail.com>
2019-04-09 21:58:35 -0500

    Makes filtering casual games less stupid ?blamevig

--- scripts/retroactivelyAddAprilGames.js
+++ scripts/retroactivelyAddAprilGames.js
@@ -9,38 +9,37 @@ Game.find({
 	date: {
 		$gte: new Date('2019-04-01 00:00:00.000'),
 		$lte: new Date('2019-04-08 00:00:00.000')
-	}
+	},
+	casualGame: false
 })
 	.cursor()
 	.eachAsync(game => {
-		if (!game.casualGame) {
-			let winnerUsernames = [],
-				loserUsernames = [];
-			game.winningPlayers.forEach(player => winnerUsernames.push(player.userName));
-			game.losingPlayers.forEach(player => loserUsernames.push(player.userName));
+		let winnerUsernames = [],
+			loserUsernames = [];
+		game.winningPlayers.forEach(player => winnerUsernames.push(player.userName));
+		game.losingPlayers.forEach(player => loserUsernames.push(player.userName));
 
-			winnerUsernames.forEach(username => {
-				Account.findOne({ username: username })
-					.cursor()
-					.eachAsync(user => {
-						console.log(username, 'win', user.winsSeason6, user.winsSeason6 ? user.winsSeason6 + 1 : 1);
-						user.winsSeason6 = user.winsSeason6 ? user.winsSeason6 + 1 : 1;
-						user.save();
-					});
-			});
+		winnerUsernames.forEach(username => {
+			Account.findOne({ username: username })
+				.cursor()
+				.eachAsync(user => {
+					console.log(username, 'win', user.winsSeason6, user.winsSeason6 ? user.winsSeason6 + 1 : 1);
+					user.winsSeason6 = user.winsSeason6 ? user.winsSeason6 + 1 : 1;
+					user.save();
+				});
+		});
 
-			loserUsernames.forEach(username => {
-				Account.findOne({ username: username })
-					.cursor()
-					.eachAsync(user => {
-						console.log(username, 'loss', user.lossesSeason6, user.lossesSeason6 ? user.lossesSeason6 + 1 : 1);
-						user.lossesSeason6 = user.lossesSeason6 ? user.lossesSeason6 + 1 : 1;
-						user.save();
-					});
-			});
-			count++;
-			if (count % 100 === 0) {
-				console.log(count + ' games processed.');
-			}
+		loserUsernames.forEach(username => {
+			Account.findOne({ username: username })
+				.cursor()
+				.eachAsync(user => {
+					console.log(username, 'loss', user.lossesSeason6, user.lossesSeason6 ? user.lossesSeason6 + 1 : 1);
+					user.lossesSeason6 = user.lossesSeason6 ? user.lossesSeason6 + 1 : 1;
+					user.save();
+				});
+		});
+		count++;
+		if (count % 100 === 0) {
+			console.log(count + ' games processed.');
 		}
 	});
