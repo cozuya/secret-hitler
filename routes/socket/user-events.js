05c56609a72f800a503d7ca4c342c68d4ebe01c8
MAX1234567890 <maxssteinberg@gmail.com>
2020-02-10 07:40:41 -0800

    Remove listed vote
    Resolves https://github.com/cozuya/secret-hitler/issues/1528

--- routes/socket/user-events.js
+++ routes/socket/user-events.js
@@ -2028,14 +2028,7 @@ module.exports.handleAddNewGameChat = (socket, passport, data, game, modUserName
 							type: 'player'
 						},
 						{
-							text: ' to vote '
-						},
-						{
-							text: `${vote ? 'ja' : 'nein'}`,
-							type: 'player'
-						},
-						{
-							text: '.'
+							text: ' to vote.'
 						}
 					]
 				});
