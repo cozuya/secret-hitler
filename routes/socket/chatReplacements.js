14e22056ee7d164f9704001439c6821a9e56c171
moira <45274875+mooira@users.noreply.github.com>
2020-02-14 22:23:34 +0300

    Corrected some spelling errors

--- routes/socket/chatReplacements.js
+++ routes/socket/chatReplacements.js
@@ -13,7 +13,7 @@ module.exports.chatReplacements = [
 		id: 2,
 		regex: /^o.verall/i,
 		replacement:
-			'If you have played 50 games but still see yourself as being grey, you might have seasonal ELO on, this will get reset with every new season. To see your overall ELO instead, go to settings and toggle “show overall winrates and colors).',
+			'If you have played 50 games but still see yourself as being grey, you might have seasonal ELO on, this will get reset with every new season. To see your overall ELO instead, go to settings and toggle “show overall winrates and colors“.',
 		aemCooldown: 15,
 		normalCooldown: 180,
 		normalGames: 50
@@ -22,7 +22,7 @@ module.exports.chatReplacements = [
 		id: 3,
 		regex: /^mod.support/i,
 		replacement:
-			'The  most effective way to contact a mod is through our discord server in #mod-support. Join here:https://discord.gg/secrethitlerio. Make sure to agree to our rules to access the channel. Once there just ping @Moderator and explain the situation. Provide game links and images when possible.',
+			'The  most effective way to contact a mod is through our discord server in #mod-support. Join here:https://discord.gg/secrethitlerio. Make sure to agree to our rules to access the channel. Once there, just ping @Moderator and explain the situation. Provide game links and images when possible.',
 		aemCooldown: 15,
 		normalCooldown: 180,
 		normalGames: 50
@@ -49,7 +49,7 @@ module.exports.chatReplacements = [
 		id: 6,
 		regex: /^d.iscard/i,
 		replacement:
-			'When you are in a government you  can either be the president or the chancellor. As the president, you receive 3 cards and select a card you do NOT want to be played. As a chancellor, you get 2 cards and select the one you WANT to play.',
+			'When you are in a government you can either be the president or the chancellor. As the president, you receive 3 cards and select a card you do NOT want to be played. As a chancellor, you get 2 cards and select the one you WANT to play.',
 		aemCooldown: 15,
 		normalCooldown: 180,
 		normalGames: 50
@@ -67,7 +67,7 @@ module.exports.chatReplacements = [
 		id: 8,
 		regex: /^b.lacklist/i,
 		replacement:
-			'To blacklist a player go to their profile, either through clicking their name on the sidebar or searching it up in settings. Once there just click "Blacklist Player". Please make sure to not threaten to, and generally avoid mentioning blacklists during games.',
+			'To blacklist a player go to their profile, either through clicking their name on the sidebar or searching it up in settings. Once there, just click "Blacklist Player". Please make sure to not threaten to, and generally avoid mentioning blacklists during games.',
 		aemCooldown: 15,
 		normalCooldown: 120,
 		normalGames: 999999999
