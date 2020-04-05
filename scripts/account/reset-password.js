2eca6d6841f3797d556c17836bc05f3a38e90868
kevin <me@kevinformatics.com>
2020-04-05 06:31:08 +0000

    Fix reset password script

--- scripts/account/reset-password.js
+++ scripts/account/reset-password.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
 const Account = require('../../models/account');
 
+const TEMP_PASSWORD = 'ChangeMe123'
+
 if (process.argv.length === 3) {
 	mongoose.Promise = global.Promise;
 	mongoose.connect(`mongodb://localhost:27017/secret-hitler-app`);
 	const username = process.argv[2];
-	const user = Account.findOne({ username });
-	user.setPassword('ChangeMe123', () => {
-		user.save();
-	});
-	console.log(`Set ${username}'s password to \'ChangeMe123\'`);
+	Account.findOne({ username }).then(user => {
+		if (user) {
+			user.setPassword(TEMP_PASSWORD, () => {
+				user.save(() => mongoose.connection.close());
+			});
+			console.log(`Set ${username}'s password to ${TEMP_PASSWORD}`);
+		} else {
+			console.log(`No user with the username ${username} found`);
+			mongoose.connection.close();
+		}
+	}).catch(err => {
+		console.error(err);
+	})
 } else if (process.argv.length > 3) {
 	console.error('Error: To many arguments');
 } else {
