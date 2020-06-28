1e96097fcc9b0bbae44da6786fa1ba9ddd86276b
Neil Chowdhury <neil.chowdhury@outlook.com>
2020-06-27 20:45:15 -0700

    make mod names persist, unless thye are seated

--- src/frontend-scripts/components/section-main/Gamechat.jsx
+++ src/frontend-scripts/components/section-main/Gamechat.jsx
@@ -648,10 +648,10 @@ class Gamechat extends React.Component {
 											: `${chat.userName} {${gameInfo.publicPlayersState.findIndex(publicPlayer => publicPlayer.userName === chat.userName) + 1}}`
 										: chat.staffRole === 'moderator' && chat.userName === 'Incognito' && canSeeIncognito
 										? chat.hiddenUsername
-										: isBlind
+										: isBlind && !isMod
 										? '?'
 										: chat.userName
-									: isBlind
+									: isBlind && (!isMod || (isMod && isSeated))
 									? '?'
 									: chat.staffRole === 'moderator' && chat.userName === 'Incognito' && canSeeIncognito
 									? chat.hiddenUsername
