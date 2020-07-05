42c7503196b041d2804f4b40e917320618307228
cozuya <chris.v.ozols@gmail.com>
2020-07-05 17:30:53 -0500

    removed non-working line graph from /stats pages and cleaned up the get data script

--- public/scripts/charts-season.js
+++ public/scripts/charts-season.js
@@ -13,120 +13,94 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 	$.ajax({
 		url: 'data',
-		success: function(data) {
-			new Chartist.Line('#chart-completed-games', {
-				labels: data.completedGames.labels,
-				series: [data.completedGames.series]
+		success: function(d) {
+			new Chartist.Pie('#chart-allplayer-games-winrate', processWinrateData(data.allPlayerGameData.fascistWinCount, data.allPlayerGameData.totalGameCount), {
+				width: '400px',
+				height: '400px'
 			});
 
-			setTimeout(() => {
-				const { labels } = data.completedGames;
-				const $labels = $('#chart-completed-games .ct-label.ct-horizontal');
-				const showingLabelIndexes = [0, Math.round(labels.length / 4), Math.round(labels.length / 2), Math.round(labels.length / 1.5), labels.length - 1];
-				const $shownlabels = $labels.filter(index => showingLabelIndexes.includes(index));
-
-				$shownlabels.show(); // barf
-			}, 500);
-
-			new Chartist.Pie(
-				'#chart-allplayer-games-winrate',
-				processWinrateData(data.allPlayerGameData.fascistWinCountSeason, data.allPlayerGameData.totalGameCountSeason),
-				{
-					width: '400px',
-					height: '400px'
-				}
+			$('#chart-allplayer-games-winrate').after(
+				`<p style="text-align: center">Total games played: ${data.allPlayerGameData.totalGameCount.toLocaleString()}</p>`
 			);
 
-			$('#chart-allplayer-games-winrate').after(`<p style="text-align: center">Total games played: ${data.allPlayerGameData.totalGameCountSeason}</p>`);
-
-			new Chartist.Pie(
-				'#chart-fiveplayer-games-winrate',
-				processWinrateData(data.fivePlayerGameData.fascistWinCountSeason, data.fivePlayerGameData.totalGameCountSeason),
-				{
-					width: '400px',
-					height: '400px'
-				}
-			);
+			new Chartist.Pie('#chart-fiveplayer-games-winrate', processWinrateData(data.fivePlayerGameData.fascistWinCount, data.fivePlayerGameData.totalGameCount), {
+				width: '400px',
+				height: '400px'
+			});
 
 			$('#chart-fiveplayer-games-winrate').after(
-				`<p style="text-align: center">Total 5 player games played: ${data.fivePlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">40%</span></p>`
+				`<p style="text-align: center">Total 5 player games played: ${data.fivePlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">40%</span></p>`
 			);
 
-			new Chartist.Pie(
-				'#chart-sixplayer-games-winrate',
-				processWinrateData(data.sixPlayerGameData.fascistWinCountSeason, data.sixPlayerGameData.totalGameCountSeason),
-				{
-					width: '400px',
-					height: '400px'
-				}
-			);
+			new Chartist.Pie('#chart-sixplayer-games-winrate', processWinrateData(data.sixPlayerGameData.fascistWinCount, data.sixPlayerGameData.totalGameCount), {
+				width: '400px',
+				height: '400px'
+			});
 
 			$('#chart-sixplayer-games-winrate').after(
-				`<p style="text-align: center">Total 6 player games played: ${data.sixPlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">33%</span></p><h2 class="ui header centered">Winrate for 6 player games (rebalanced)</h2><div class="chart" id="chart-sixplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 6 player rebalanced games played: ${data.sixPlayerGameData.rebalancedTotalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">33%</span></p>`
+				`<p style="text-align: center">Total 6 player games played: ${data.sixPlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">33%</span></p><h2 class="ui header centered">Winrate for 6 player games (rebalanced)</h2><div class="chart" id="chart-sixplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 6 player rebalanced games played: ${
+					data.sixPlayerGameData.rebalancedTotalGameCount
+				} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">33%</span></p>`
 			);
 
 			new Chartist.Pie(
 				'#chart-sixplayer-rebalanced-games-winrate',
-				processWinrateData(data.sixPlayerGameData.rebalancedFascistWinCountSeason, data.sixPlayerGameData.rebalancedTotalGameCountSeason),
+				processWinrateData(data.sixPlayerGameData.rebalancedFascistWinCount, data.sixPlayerGameData.rebalancedTotalGameCount),
 				{ width: '400px', height: '400px' }
 			);
 
 			new Chartist.Pie(
 				'#chart-sevenplayer-games-winrate',
-				processWinrateData(data.sevenPlayerGameData.fascistWinCountSeason, data.sevenPlayerGameData.totalGameCountSeason),
+				processWinrateData(data.sevenPlayerGameData.fascistWinCount, data.sevenPlayerGameData.totalGameCount),
 				{ width: '400px', height: '400px' }
 			);
 
 			$('#chart-sevenplayer-games-winrate').after(
-				`<p style="text-align: center">Total 7 player games played: ${data.sevenPlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">43%</span></p><h2 class="ui header centered">Winrate for 7 player games (rebalanced)</h2><div class="chart" id="chart-sevenplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 7 player rebalanced games played: ${data.sevenPlayerGameData.rebalancedTotalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">43%</span></p>`
+				`<p style="text-align: center">Total 7 player games played: ${data.sevenPlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">43%</span></p><h2 class="ui header centered">Winrate for 7 player games (rebalanced)</h2><div class="chart" id="chart-sevenplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 7 player rebalanced games played: ${
+					data.sevenPlayerGameData.rebalancedTotalGameCount
+				} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">43%</span></p>`
 			);
 
 			new Chartist.Pie(
 				'#chart-sevenplayer-rebalanced-games-winrate',
-				processWinrateData(data.sevenPlayerGameData.rebalancedFascistWinCountSeason, data.sevenPlayerGameData.rebalancedTotalGameCountSeason),
+				processWinrateData(data.sevenPlayerGameData.rebalancedFascistWinCount, data.sevenPlayerGameData.rebalancedTotalGameCount),
 				{ width: '400px', height: '400px' }
 			);
 
 			new Chartist.Pie(
 				'#chart-eightplayer-games-winrate',
-				processWinrateData(data.eightPlayerGameData.fascistWinCountSeason, data.eightPlayerGameData.totalGameCountSeason),
+				processWinrateData(data.eightPlayerGameData.fascistWinCount, data.eightPlayerGameData.totalGameCount),
 				{ width: '400px', height: '400px' }
 			);
 
 			$('#chart-eightplayer-games-winrate').after(
-				`<p style="text-align: center">Total 8 player games played: ${data.eightPlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">38%</span></p>`
+				`<p style="text-align: center">Total 8 player games played: ${data.eightPlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">38%</span></p>`
 			);
 
-			new Chartist.Pie(
-				'#chart-nineplayer-games-winrate',
-				processWinrateData(data.ninePlayerGameData.fascistWinCountSeason, data.ninePlayerGameData.totalGameCountSeason),
-				{
-					width: '400px',
-					height: '400px'
-				}
-			);
+			new Chartist.Pie('#chart-nineplayer-games-winrate', processWinrateData(data.ninePlayerGameData.fascistWinCount, data.ninePlayerGameData.totalGameCount), {
+				width: '400px',
+				height: '400px'
+			});
 
 			$('#chart-nineplayer-games-winrate').after(
-				`<p style="text-align: center">Total 9 player games played: ${data.ninePlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">44%</span></p><h2 class="ui header centered">Winrate for 9 player games (rebalanced)</h2><div class="chart" id="chart-nineplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 9 player rebalanced games played: ${data.ninePlayerGameData.rebalanced2fFascistWinCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">44%</span></p>`
+				`<p style="text-align: center">Total 9 player games played: ${data.ninePlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">44%</span></p><h2 class="ui header centered">Winrate for 9 player games (rebalanced)</h2><div class="chart" id="chart-nineplayer-rebalanced-games-winrate"></div><p style="text-align: center">Total 9 player rebalanced games played: ${
+					data.ninePlayerGameData.rebalanced2fFascistWinCount
+				} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">44%</span></p>`
 			);
 
 			new Chartist.Pie(
 				'#chart-nineplayer-rebalanced-games-winrate',
-				processWinrateData(data.ninePlayerGameData.rebalanced2fFascistWinCountSeason, data.ninePlayerGameData.rebalanced2fTotalGameCountSeason),
+				processWinrateData(data.ninePlayerGameData.rebalanced2fFascistWinCount, data.ninePlayerGameData.rebalanced2fTotalGameCount),
 				{ width: '400px', height: '400px' }
 			);
 
-			new Chartist.Pie(
-				'#chart-tenplayer-games-winrate',
-				processWinrateData(data.tenPlayerGameData.fascistWinCountSeason, data.tenPlayerGameData.totalGameCountSeason),
-				{
-					width: '400px',
-					height: '400px'
-				}
-			);
+			new Chartist.Pie('#chart-tenplayer-games-winrate', processWinrateData(data.tenPlayerGameData.fascistWinCount, data.tenPlayerGameData.totalGameCount), {
+				width: '400px',
+				height: '400px'
+			});
 
 			$('#chart-tenplayer-games-winrate').after(
-				`<p style="text-align: center">Total 10 player games played: ${data.tenPlayerGameData.totalGameCountSeason} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">40%</span></p>`
+				`<p style="text-align: center">Total 10 player games played: ${data.tenPlayerGameData.totalGameCount.toLocaleString()} | Percentage of Fascists in game: <span style="color: red; font-weight: bold">40%</span></p>`
 			);
 		}
 	});
