d32d70997a1f8802cab4001dc5a48d9b8fe7fd64
Neil Chowdhury <neil.chowdhury@outlook.com>
2020-09-05 19:59:10 -0700

    test that emote filenames are valid

--- scripts/processEmotes.test.js
+++ scripts/processEmotes.test.js
@@ -12,3 +12,17 @@ test('all emote images are 28x28', done => {
 	});
 	done();
 });
+
+test('all emote filenames are valid', done => {
+	fs.readdirSync(`public/images/emotes`, { withFileTypes: true }).forEach(file => {
+		if (file.isFile()) {
+			if (file.name.charAt(0) !== file.name.charAt(0).toUpperCase()) {
+				done.fail(`Emote ${file.name} should begin with an uppercase letter`);
+			}
+			if (!file.name.endsWith('.png')) {
+				done.fail(`Emote ${file.name} is an invalid file type. Only PNG files are allowed.`);
+			}
+		}
+	});
+	done();
+});
