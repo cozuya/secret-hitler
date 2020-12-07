e7d444c6d5d10e69253cc0ca668a2c35a3fe650b
Vignesh Joglekar <rexvigasaurus@gmail.com>
2020-12-07 22:59:55 +0000

    Include IP matches in automated reports (#1685)
    Squashed commit of the following:
    
    commit 6025745ffb6c68969c2a7b323a89ed73165a73a3
    Author: Vignesh Joglekar <rexvigasaurus@gmail.com>
    Date:   Mon Dec 7 22:54:33 2020 +0000
    
        This already has a guard clause
    
    commit fddaf56ac089e0ec8fed983b6768e3d40f8cbc30
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Mon Nov 2 12:18:09 2020 -0800
    
        i'm not able to test this since I don't have access to a deployed dev site where I can connect on a 3-block match, but this should work in theory :blobsweat:
    
    commit 5188fdec77cd03642deb997e631b33bf07d6a05b
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Mon Nov 2 11:23:40 2020 -0800
    
        lets not crash site (also i can't really do 3-block matches without not supporting IPv6)
    
    commit 6a8386174ecad0d8444afe838afba8f425fa8fc5
    Merge: 2a60c591 2429ac03
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Mon Oct 5 10:41:41 2020 -0700
    
        Merge remote-tracking branch 'origin/include-ips-in-autos' into include-ips-in-autos
    
    commit 2a60c591158d700319781d3b124701761c61af8f
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Mon Oct 5 10:41:06 2020 -0700
    
        creates a distinction between Fascist and Hitler in auto reports
    
    commit 2429ac0330e12d46f08170e73dbf3948adf7a3fc
    Merge: d024dbb4 9efee038
    Author: MAX1234567890 <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Wed Sep 30 12:09:56 2020 -0700
    
        Merge branch 'v1.7.1' into include-ips-in-autos
    
    commit d024dbb4f5aedb1cc57b718d1b935dc97d85ddc2
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Sat Aug 15 22:57:41 2020 -0700
    
        integrates other PR
    
    commit e9c238d3ca64f4b9efc97efed2a1dd54d11b31f2
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Sat Aug 15 22:54:18 2020 -0700
    
        completed all requested changes but one
    
    commit 45737a57c140d73050787d88b6e9b1bd82a52f5a
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Thu Jul 30 16:49:48 2020 -0700
    
        lmao forgot to remove console.log
    
    commit 6c7b565c48da80afbba128371296caa53958b4f4
    Author: Paladin of Ioun <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Thu Jul 30 16:44:14 2020 -0700
    
        Adds information on matching IPs to automated reports.
        I also moved around some code to make it a bit easier to work with autos in the future.
    
    commit 9a252fc352913af5a61ec5dad9ef909f8c410619
    Merge: c36f664f b699f83c
    Author: MAX1234567890 <16229400+MAX1234567890@users.noreply.github.com>
    Date:   Thu Jul 30 14:13:53 2020 -0700
    
        Merge pull request #3 from cozuya/master
    
        aaaa
    
    commit c36f664f53d7f9950ae3c6609d59375f34851bae
    Merge: 68ddbaa1 f84ab496
    Author: MAX1234567890 <maxssteinberg@gmail.com>
    Date:   Fri Jul 10 14:08:28 2020 -0700
    
        Merge pull request #2 from cozuya/master
    
        a
    
    commit 68ddbaa188a24b11bd72aaa426d346d4e3cdeb25
    Author: Max Steinberg <maxssteinberg@gmail.com>
    Date:   Thu Jan 16 17:35:13 2020 -0800
    
        Fixed filename
    
    commit b24cd4d50cee91f2291bb355f67de4563d70ba2b
    Author: Max Steinberg <maxssteinberg@gmail.com>
    Date:   Thu Jan 16 17:32:08 2020 -0800
    
        Added api dump script.
    
    commit bd1225a8589a5a5132b0a6e999345673ffa3d6ce
    Merge: 48565c23 394b57a3
    Author: MAX1234567890 <maxssteinberg@gmail.com>
    Date:   Thu Jan 16 14:57:21 2020 -0800
    
        Merge pull request #1 from cozuya/master
    
        test

--- routes/socket/report.js
+++ routes/socket/report.js
@@ -2,76 +2,7 @@ const https = require('https');
 const Account = require('../../models/account');
 const { newStaff } = require('./models');
 
-module.exports.makeReport = (data, game, type = 'report') => {
-	// No Auto-Reports, or Mod Pings from Custom, Unlisted, or Private Games
-	if (!game || game.customGameSettings.enabled || game.general.unlisted || game.general.private) return;
-	const { player, seat, role, election, situation, uid, gameType } = data;
-
-	let report;
-
-	if (type === 'ping') {
-		report = JSON.stringify({
-			content: `${process.env.DISCORDMODPING}\n__**Player**__: ${player} \n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
-			username: '@Mod Ping',
-			avatar_url: 'https://cdn.discordapp.com/emojis/612042360318328842.png?v=1'
-		});
-		if (process.env.NODE_ENV === 'production') {
-			try {
-				const req = https.request({
-					hostname: 'discordapp.com',
-					path: process.env.DISCORDREPORTURL,
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Content-Length': Buffer.byteLength(report)
-					}
-				});
-				req.end(report);
-			} catch (e) {
-				console.log(e);
-			}
-		} else {
-			console.log(`${text}\n${game.general.uid}`);
-		}
-		return;
-	}
-
-	if (type === 'reportdelayed') {
-		report = JSON.stringify({
-			content: `${process.env.DISCORDMODPING} - **AEM DELAYED**\n__**Player**__: ${player} {${seat}}\n__**Role**__: ${role}\n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
-			username: 'Auto Report',
-			avatar_url: 'https://cdn.discordapp.com/emojis/230161421336313857.png?v=1'
-		});
-	}
-
-	if (type === 'modchatdelayed') {
-		report = JSON.stringify({
-			content: `${process.env.DISCORDMODPING} - **AEM DELAYED**\n__**Member**__: ${player} \n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
-			username: 'Mod Chat',
-			avatar_url: 'https://cdn.discordapp.com/emojis/230161421311148043.png?v=1'
-		});
-	}
-
-	if (type === 'modchat') {
-		report = JSON.stringify({
-			content: `${process.env.DISCORDMODPING}\n__**Member**__: ${player} \n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
-			username: 'Mod Chat',
-			avatar_url: 'https://cdn.discordapp.com/emojis/230161421311148043.png?v=1'
-		});
-	}
-
-	if (type === 'report' && !game.general.casualGame) {
-		report = JSON.stringify({
-			content: `${process.env.DISCORDMODPING}\n__**Player**__: ${player} {${seat}}\n__**Role**__: ${role}\n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
-			username: 'Auto Report',
-			avatar_url: 'https://cdn.discordapp.com/emojis/230161421336313857.png?v=1'
-		});
-	}
-
-	if (type === 'report' || type === 'modchat') {
-		game.private.hiddenInfoShouldNotify = false;
-	}
-
+function sendReport(game, report, data, type) {
 	Account.find({ staffRole: { $exists: true } }).then(accounts => {
 		const staffUserNames = accounts
 			.filter(
@@ -104,6 +35,7 @@ module.exports.makeReport = (data, game, type = 'report') => {
 
 		if (process.env.NODE_ENV === 'production') {
 			try {
+				report = JSON.stringify(report);
 				const req = https.request({
 					hostname: 'discordapp.com',
 					path: process.env.DISCORDREPORTURL,
@@ -118,7 +50,125 @@ module.exports.makeReport = (data, game, type = 'report') => {
 				console.log(e);
 			}
 		} else {
-			console.log(`${report}\n${game.general.uid}`);
+			const text = JSON.stringify(report);
+			console.log(`${text}\n${game.general.uid}`);
 		}
 	});
+}
+
+module.exports.makeReport = (data, game, type = 'report') => {
+	// No Auto-Reports, or Mod Pings from Custom, Unlisted, or Private Games
+	if (!game || game.customGameSettings.enabled || game.general.unlisted || game.general.private) return;
+	// No Auto-Reports from Casual games
+	if (game.general.casualGame && (type === 'report' || type === 'reportdelayed')) return;
+	const { player, seat, role, election, situation, uid, gameType } = data;
+
+	let report;
+
+	if (type === 'report' || type === 'modchat') {
+		game.private.hiddenInfoShouldNotify = false;
+	}
+
+	if (type === 'ping') {
+		report = JSON.stringify({
+			content: `${process.env.DISCORDMODPING}\n__**Player**__: ${player} \n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
+			username: '@Mod Ping',
+			avatar_url: 'https://cdn.discordapp.com/emojis/612042360318328842.png?v=1'
+		});
+		if (process.env.NODE_ENV === 'production') {
+			try {
+				const req = https.request({
+					hostname: 'discordapp.com',
+					path: process.env.DISCORDREPORTURL,
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						'Content-Length': Buffer.byteLength(report)
+					}
+				});
+				req.end(report);
+			} catch (e) {
+				console.log(e);
+			}
+		} else {
+			console.log(`${text}\n${game.general.uid}`);
+		}
+		return;
+	}
+
+	if (type === 'report' || type === 'reportdelayed') {
+		const upperRole = role[0].toUpperCase() + role.substr(1);
+		const isDelayed = type === 'reportdelayed' ? ' - **AEM DELAYED**' : '';
+		let throwerIP;
+		const otherPlayers = [];
+
+		report = {
+			content: `${process.env.DISCORDMODPING}${isDelayed}\n__**Player**__: ${player} {${seat}}\n__**Role**__: ${upperRole}\n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}`,
+			username: 'Auto Report',
+			avatar_url: 'https://cdn.discordapp.com/emojis/230161421336313857.png?v=1'
+		};
+
+		game.publicPlayersState.map(state => {
+			if (state.userName !== player) {
+				otherPlayers.push(state.userName);
+			}
+		});
+
+		Account.findOne({ username: player }, (err, account) => {
+			if (err) console.log(err, 'err finding user');
+			else if (account) data.ip = account.lastConnectedIP || account.signupIP;
+			throwerIP = data.ip;
+
+			const matches = {};
+			Account.find({ username: { $in: otherPlayers } })
+				.then(accounts => {
+					accounts.forEach(account => {
+						let ip;
+						if (account) ip = account.lastConnectedIP || account.signupIP;
+
+						const seat = game.private.seatedPlayers.findIndex(elem => elem.userName === account.username);
+						if (ip === throwerIP) {
+							matches[seat] = `${account.username} {${seat + 1}}`;
+						} else if (
+							ip.includes('.') && // Ensure both IPs are IPv4
+							throwerIP.includes('.') &&
+							ip
+								.split('.')
+								.splice(0, -1)
+								.join('.') === // Splice off last block
+								throwerIP
+									.split('.')
+									.splice(0, -1)
+									.join('.') // to determine if the IPs are a 3-block match
+						) {
+							matches[seat] = `${account.username} {${seat + 1}} (3-block)`;
+						}
+					});
+				})
+				.then(() => {
+					const sortedSeats = Object.keys(matches).sort();
+					if (sortedSeats.length > 0) {
+						const sortedMatches = [];
+
+						for (const seat of sortedSeats) {
+							sortedMatches.push(matches[seat]);
+						}
+
+						report.content += `\n__**Matching IPs**__: ${sortedMatches.join(', ')}`;
+					}
+					report.content += `\n**<https://secrethitler.io/game/#/table/${uid}>**`;
+					sendReport(game, report, data, type);
+				});
+		});
+	}
+
+	if (type === 'modchat' || type === 'modchatdelayed') {
+		const isDelayed = type === 'modchatdelayed' ? ' - **AEM DELAYED**' : '';
+		report = {
+			content: `${process.env.DISCORDMODPING}${isDelayed}\n__**Member**__: ${player} \n__**Situation**__: ${situation}\n__**Election #**__: ${election}\n__**Game Type**__: ${gameType}\n**<https://secrethitler.io/game/#/table/${uid}>**`,
+			username: 'Mod Chat',
+			avatar_url: 'https://cdn.discordapp.com/emojis/230161421311148043.png?v=1'
+		};
+		sendReport(game, report, data, type);
+	}
 };
