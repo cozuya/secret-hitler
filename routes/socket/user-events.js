6c8f5a74cb5dc4a401ca339a47954bcc0a449888
Paladin <16229400+MAX1234567890@users.noreply.github.com>
2021-03-12 14:40:45 -0800

    alfalfa

--- routes/socket/user-events.js
+++ routes/socket/user-events.js
@@ -2219,24 +2219,35 @@ module.exports.handleAddNewGameChat = (socket, passport, data, game, modUserName
 			}
 			io.sockets.sockets[affectedSocketId].emit(
 				'pingPlayer',
-				game.general.blindMode ? 'Secret Hitler IO: A player has pinged you.' : `Secret Hitler IO: Player ${data.userName} just pinged you.`
+				game.general.blindMode || game.general.playerChats === 'disabled'
+					? 'Secret Hitler IO: A player has pinged you.'
+					: `Secret Hitler IO: Player ${data.userName} just pinged you.`
 			);
 
-			game.chats.push({
-				gameChat: true,
-				userName: passport.user,
-				timestamp: new Date(),
-				chat: [
-					{
-						text: game.general.blindMode
-							? `A player has pinged player number ${affectedPlayerNumber + 1}.`
-							: `${passport.user} has pinged ${publicPlayersState[affectedPlayerNumber].userName} (${affectedPlayerNumber + 1}).`
-					}
-				],
-				previousSeasonAward: user.previousSeasonAward,
-				uid: data.uid,
-				inProgress: game.gameState.isStarted
-			});
+			if (game.general.playerChats === 'disabled') {
+				player.gameChats.push({
+					timestamp: new Date(),
+					gameChat: true,
+					chat: [{ text: game.publicPlayersState[affectedPlayerNumber].userName, type: 'player' }, { text: ' has been successfully pinged.' }]
+				});
+			} else {
+				game.chats.push({
+					gameChat: true,
+					userName: passport.user,
+					timestamp: new Date(),
+					chat: [
+						{
+							text: game.general.blindMode
+								? `A player has pinged player number ${affectedPlayerNumber + 1}.`
+								: `${passport.user} has pinged ${publicPlayersState[affectedPlayerNumber].userName} (${affectedPlayerNumber + 1}).`
+						}
+					],
+					previousSeasonAward: user.previousSeasonAward,
+					uid: data.uid,
+					inProgress: game.gameState.isStarted
+				});
+			}
+
 			sendInProgressGameUpdate(game);
 		} catch (e) {
 			console.log(e, 'caught exception in ping chat');
