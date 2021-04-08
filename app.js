34d688d946ced3f0149980a4c7b879a754c7bb16
Paladin <16229400+MAX1234567890@users.noreply.github.com>
2021-04-07 17:34:56 -0700

    Revert "replaces file upload with"
    This reverts commit cf86efb30b429f8b2c25c4a6acb9ec62e95dec33.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ let store;
 if (process.env.NODE_ENV !== 'production') {
 	const MongoDBStore = require('connect-mongodb-session')(session);
 	store = new MongoDBStore({
-		uri: process.env.MONGO_URL,
+		uri: 'mongodb://localhost:27017/secret-hitler-app',
 		collection: 'sessions'
 	});
 } else {
@@ -133,7 +133,7 @@ if (process.env.DISCORDCLIENTID) {
 
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://localhost:27017/secret-hitler-app`, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
