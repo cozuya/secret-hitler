b261a941135eaa3d3f1bd3f6f0b81d69eb087c9a
Vignesh Joglekar <rexvigasaurus@gmail.com>
2021-10-18 10:56:37 -0500

    Applies season end date hotfix

--- src/frontend-scripts/components/section-main/GamesList.jsx
+++ src/frontend-scripts/components/section-main/GamesList.jsx
@@ -238,7 +238,7 @@ export class GamesList extends React.Component {
 				<a href="#/changelog">
 					<h5 title="A season is an optional new tier of elo that is reset every 3 months.">
 						{new Date() > new Date('2021-10-04')
-							? `Season ends ${moment(new Date('2021-01-03T05:00:00.000Z')).fromNow()}.`
+							? `Season ends ${moment(new Date('2022-01-03T05:00:00.000Z')).fromNow()}.`
 							: `Welcome to season ${CURRENTSEASONNUMBER}!`}
 					</h5>
 				</a>
