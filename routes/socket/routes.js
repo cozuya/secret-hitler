efa5c0980a77eb0e722ba6583db07a95c0f66418
Vignesh Joglekar <rexvigasaurus@gmail.com>
2022-05-25 14:46:18 -0500

    Reenabled observe socket, temporarily

--- routes/socket/routes.js
+++ routes/socket/routes.js
@@ -208,9 +208,6 @@ module.exports.socketRoutes = () => {
 					if (account.staffRole && account.staffRole.length > 0 && account.staffRole === 'trialmod') isTrial = true;
 					if (account.isTournamentMod) isTourneyMod = true;
 				});
-			} else {
-				socket.disconnect();
-				return;
 			}
 
 			sendGeneralChats(socket);
